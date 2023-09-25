0bca339d4f3dfdb010f5c0f413177e45e5762b3e
Praveen2415 <139866628+Praveen2415@users.noreply.github.com>
2023-09-25 22:02:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+const foodJson = require("./food.json");
+
+// list of all foodnames
+let all_items = foodJson.map(items=>{
+    return (items.foodname);
+});
+console.log(all_items);
+
+// list of all vegetable
+let all_vegetable_items = foodJson.filter(items =>{
+    return (items.category === "Vegetable");
+});
+console.log(all_vegetable_items);
+
+//list of all fruits
+let all_fruits_items = foodJson.filter(items =>{
+    return(items.category === "Fruit");
+});
+console.log(all_fruits_items);
+
+// list of all proteins
+let all_proteins_items = foodJson.filter(items =>{
+    return (items.category === "Protein");
+});
+console.log(all_proteins_items);
+
+//list of all nuts
+let all_nuts_items = foodJson.filter(items =>{
+    return(items.category === "Nuts");
+});
+console.log(all_nuts_items);
+
+//list of all grains
+let all_grains_items = foodJson.filter(items=>{
+    return(items.category === "Grain");
+});
+console.log(all_grains_items);
+
+// list of all dairy
+let all_dairy_items = foodJson.filter(items=>{
+    return(items.category === "Dairy");
+});
+console.log(all_dairy_items);
+
+// list of protein above 100
+let high_proteins = foodJson.filter(items=>{
+    return(items.calorie>100);
+});
+console.log(high_proteins);
+
+// list of protein below 100
+let low_proteins = foodJson.filter(items=>{
+    return(items.calorie<100);
+});
+console.log(low_proteins);
+
+//list of high to low protein order
+let sort_protein = foodJson.sort((low,high)=>{
+    return (high.proteins - low.proteins);
+});
+console.log(sort_protein);
+
+//list of low to high cabs
+let sort_cabs = foodJson.sort((low,high)=>{
+    return (low.cab - high.cab );
+});
+console.log(sort_cabs);
